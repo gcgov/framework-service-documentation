b0f8a7cf36afd5711d51f2c32a4934969aa6f3c6
Andrew Sauder <andrewsauder@gmail.com>
2023-12-27 10:19:43 -0500

    improve documentation scanning

--- src/controllers/documentation.php
+++ src/controllers/documentation.php
@@ -16,12 +16,63 @@ class documentation implements controller {
 
 	#[NoReturn]
 	public function yaml(): void {
-		$openapi = \OpenApi\Generator::scan( [ config::getRootDir() . '/app', config::getRootDir() . '/vendor/gcgov/framework/src/controllers', config::getRootDir() . '/vendor/gcgov/framework/src/exceptions', config::getRootDir() . '/vendor/gcgov/framework/src/models', config::getRootDir() . '/vendor/gcgov/framework/src/services' ], [ 'exclude' => [ 'vendor' ] ] );
+		$scanDirectories = $this->getScanDirectories();
+		$excludeFilesDirectories = $this->getExcludeDirectoriesFiles();
+		$files = \OpenApi\Util::finder($scanDirectories, $excludeFilesDirectories, '*.php');
+		$openapi           = \OpenApi\Generator::scan( $files );
 		header( 'Content-Type: text/x-yaml' );
 		echo $openapi->toYaml();
 		die();
 	}
 
+
+	/**
+	 * @return string[]
+	 */
+	private function getScanDirectories(): array {
+		$directoriesToScan = [
+			config::getAppDir(),
+			config::getRootDir() . '/vendor/gcgov/framework/src/controllers',
+			config::getRootDir() . '/vendor/gcgov/framework/src/exceptions',
+			config::getRootDir() . '/vendor/gcgov/framework/src/models',
+			config::getRootDir() . '/vendor/gcgov/framework/src/services'
+		];
+
+		foreach( $directoriesToScan as $i=>$directory ) {
+			if(!file_exists($directory)) {
+				unset($directoriesToScan[$i]);
+			}
+		}
+
+		return array_values( $directoriesToScan );
+	}
+
+
+	/**
+	 * @return string[]
+	 */
+	private function getExcludeDirectoriesFiles(): array {
+		$exclusions = [
+			config::getRootDir() . '/vendor',
+		];
+
+		foreach( $exclusions as $i=>$exclude ) {
+			if(!file_exists($exclude)) {
+				unset($exclusions[$i]);
+			}
+		}
+
+		if( class_exists('\app\models\user') ) {
+			$exclusions[] = config::getRootDir() . '/vendor/gcgov/framework/src/services/mongodb/models/auth/user.php';
+		}
+
+		if( class_exists('\app\models\authUser') ) {
+			$exclusions[] = config::getRootDir() . '/vendor/gcgov/framework/src/models/authUser.php';
+		}
+
+		error_log(json_encode(array_values($exclusions)));
+		return array_values( $exclusions );
+	}
 
 	public function routes(): controllerDataResponse {
 		$routes = [];
